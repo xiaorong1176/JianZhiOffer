a061524eeaea05351db76ab32f8c7de97a2452d6
xiaorong1176 <750106615@qq.com>
2015-06-06 17:41:11 +0800

    return 1 << (int) (target - 1);

--- src/com/echo/frogjump/FrogJump.java
+++ src/com/echo/frogjump/FrogJump.java
@@ -24,12 +24,12 @@ public class FrogJump {
      * 题目描述：一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
      */
     public static int JumpFloorII(int target) {
-    	if(target<=0)
+/*    	if(target<=0)
     		return 0;
     	if(target == 1)
     		return 1;
-    	
-    	
-		return 2*JumpFloorII(target-1);
+    	  	
+		return 2*JumpFloorII(target-1);*/
+    	return 1 << (int) (target - 1);
     }
 }
