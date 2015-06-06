969fc6bd55fc56c74a8966b598d3cc06e6b3f82c
xiaorong1176 <750106615@qq.com>
2015-06-06 17:38:42 +0800

    变态跳台阶

--- src/com/echo/frogjump/FrogJump.java
+++ src/com/echo/frogjump/FrogJump.java
@@ -2,7 +2,7 @@ package com.echo.frogjump;
 
 public class FrogJump {
 	public static void main(String[] args) {
-		int num = JumpFloor(9);
+		int num = JumpFloorII(9);
 		System.out.println(num);
 	}
 	
@@ -18,5 +18,18 @@ public class FrogJump {
     		return 2;
     	
 		return JumpFloor(target-1) + JumpFloor(target-2);
+    }
+    
+    /*
+     * 题目描述：一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
+     */
+    public static int JumpFloorII(int target) {
+    	if(target<=0)
+    		return 0;
+    	if(target == 1)
+    		return 1;
+    	
+    	
+		return 2*JumpFloorII(target-1);
     }
 }
